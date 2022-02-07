8ce40840a037530c0354b0004d9e5470e358f634
Scott Lougheed <scott@scottlougheed.com>
2022-02-07 13:09:46 -0800

    adding readme file and skeletal code

--- provisioning.sh
+++ provisioning.sh
@@ -1,1 +1,10 @@
 #!/bin/bash
+
+# this script will ingest a .csv file containing names and email addresses and invite each person to join the currently-logged in 1Password account. 
+
+people=<List of people from .csv>
+
+for person in $people
+do
+	op user invite --name "[NAME]" --email "[EMAIL]"
+done
